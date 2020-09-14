d9672a4af9e07f604e9628354bbf95b97023302f
sehui-byte <64109506+sehui-byte@users.noreply.github.com>
2020-09-15 00:03:02 +0900

    Update BOJ2960.java
    다른 풀이방식 업로드

--- BOJ2960.java
+++ BOJ2960.java
@@ -5,36 +5,23 @@ public class BOJ2960 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int k = sc.nextInt();
-		int arr[] = new int[n];
+		boolean arr[] = new boolean[n+1];
 		int count = 0;
 
-		for(int i = 2; i<=n; i++) 
-			arr[i-2] = i;
-
-		for(int j = 0; j<k; j++) {
-			int p = 0;
-			for(int i = 0; i<n; i++) {
-				if(arr[i] != 0) {
-					p = arr[i];
-					count++;
-					if(count == k)
-						System.out.println(arr[i]);
-					arr[i] = 0; //p를 지운다
-					break;
+		for(int i = 2; i<= n; i++) {
+			if(!arr[i]) {
+				int p = i;
+				for(int j = p; j<= n; j++) {
+					if(!arr[j] && j%p == 0) {
+						arr[j] = true;
+						count++;
+						if(count == k) {
+							System.out.println(j); 
+							return;
+						}
+					}
 				}
 			}
-
-			for(int i = 0; i<n; i++) {
-				if(arr[i] == 0)
-					continue;
-				if(arr[i]%p == 0) {
-					count++;
-					if(count == k)
-						System.out.println(arr[i]);
-					arr[i] = 0; //p의 배수들을 지운다
-					
-				}
-			}	
 		}
 	}
 }
